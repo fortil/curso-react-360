ffc7199b0d70d30a016e7935e50e0993035d39ef
William Penagos <williampenagos@Williams-MacBook-Pro.local>
2018-09-05 15:56:31 -0500

    modified the client and index files

--- client.js
+++ client.js
@@ -1,23 +1,31 @@
-// This file contains the boilerplate to execute your React app.
-// If you want to modify your application's content, start in "index.js"
-
-import {ReactInstance} from 'react-360-web';
+import {ReactInstance, Location, Surface} from 'react-360-web';
 
 function init(bundle, parent, options = {}) {
   const r360 = new ReactInstance(bundle, parent, {
-    // Add custom options here
     fullScreen: true,
     ...options,
   });
 
-  // Render your app content to the default cylinder surface
+  // Create three roots: two flat panels on the left and the right, and a Location
+  // to mount rendered models in 3D space
+  const leftPanel = new Surface(300, 600, Surface.SurfaceShape.Flat);
+  leftPanel.setAngle(-0.6, 0);
+  const rightPanel = new Surface(300, 600, Surface.SurfaceShape.Flat);
+  rightPanel.setAngle(0.6, 0);
   r360.renderToSurface(
-    r360.createRoot('Hello360', { /* initial props */ }),
-    r360.getDefaultSurface()
+    r360.createRoot('TopPosts'),
+    leftPanel,
+  );
+  r360.renderToSurface(
+    r360.createRoot('CurrentPost'),
+    rightPanel,
+  );
+  r360.renderToLocation(
+    r360.createRoot('ModelView'),
+    new Location([0, -2, -10]),
   );
 
-  // Load the initial environment
-  r360.compositor.setBackground(r360.getAssetURL('360_world.jpg'));
+  r360.compositor.setBackground('./static_assets/360_world.jpg');
 }
 
 window.React360 = {init};
--- index.js
+++ index.js
@@ -1,47 +1,10 @@
-import React from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-360';
+import {AppRegistry} from 'react-360';
+import TopPosts from './TopPosts';
 import CurrentPost from './CurrentPost';
+import ModelView from './ModelView';
 import * as Store from './Store';
 Store.initialize('API KEY HERE');
 
-export default class Hello360 extends React.Component {
-  render() {
-    return (
-      <View style={styles.panel}>
-        <View style={styles.greetingBox}>
-          <Text style={styles.greeting}>
-            Welcome to React 360 in Lima
-          </Text>
-        </View>
-      </View>
-    );
-  }
-};
-
-const styles = StyleSheet.create({
-  panel: {
-    // Fill the entire surface
-    width: 1000,
-    height: 600,
-    backgroundColor: 'rgba(255, 255, 255, 0.4)',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  greetingBox: {
-    padding: 20,
-    backgroundColor: '#000000',
-    borderColor: '#639dda',
-    borderWidth: 2,
-  },
-  greeting: {
-    fontSize: 30,
-  },
-});
-
+AppRegistry.registerComponent('TopPosts', () => TopPosts);
 AppRegistry.registerComponent('CurrentPost', () => CurrentPost);
-AppRegistry.registerComponent('Hello360', () => Hello360);
+AppRegistry.registerComponent('ModelView', () => ModelView);
